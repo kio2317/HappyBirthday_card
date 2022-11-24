0fd56a331986a9b21699ebca5344f05e393e8cef
kio2317 <kio2317@naver.com>
2022-11-24 13:45:21 +0900

    commit

---
+++ app/src/main/java/com/example/happybrithday/uiman.kt
@@ -0,0 +1,9 @@
+package com.example.happybrithday
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.Composable
+
+
+class uiman : ComponentActivity(){
+    override fun oncreate
+}
